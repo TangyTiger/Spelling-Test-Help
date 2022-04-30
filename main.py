a974ad6a2a08d51d42bbd700c2ae6d933c6482fa
TangyTiger <shahaditya@outlook.com>
2022-04-29 21:04:44 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pyttsx3
+import random
+import time
+
+wrong = []
+engine = pyttsx3.init()
+
+
+words = []
+with open('spellinglist.txt') as f:
+    words = f.readlines()
+
+temp_words = words
+
+while len(temp_words) > 0: 
+    rand = random.randint(0, len(temp_words)-1)
+    engine.say(temp_words[rand])
+    engine.runAndWait()
+    inputed = input("Spell: ").lower()+"\n"
+    if inputed != temp_words[rand]:
+        print("Incorrect")
+        wrong.append(temp_words[rand])
+    temp_words.pop(rand)
+
+
+if len(wrong) == 0:
+    print("None wrong!")
+else:
+    print("Wrong: ", wrong)
